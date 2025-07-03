939247e3802c63e861e3eaf1c1c7b9d6f0288a64
Maksym Shmelyov <mailbox9111@gmail.com>
2025-07-03 13:44:50 +0200

    pages

--- src/app/features/users/services/user.service.ts
+++ src/app/features/users/services/user.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user.interface';
+// TODO: alias
+import { environment } from '../../../../env/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient) {}
+  private apiUrl = environment.apiUrl;
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>('/data/users.json');
+    return this.http.get<User[]>(`${this.apiUrl}/data/users.json`);
   }
 }
